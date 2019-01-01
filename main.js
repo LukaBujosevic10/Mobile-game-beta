7b66b613bd2f5d59e505113ec5d0d07c66c949c9
LukaBujosevic10 <lukabujosevic@gmail.com>
2019-01-01 15:24:56 +0100

    b

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 'use strict'
 $(document).ready(function() {
+  let poz_x;
+  let poz_y;
+  let ostatak_x;
+  let ostatak_y;
   let canvas = document.getElementById('canvas');
   let ctx = canvas.getContext('2d');
   let element;
@@ -37,13 +41,11 @@ $(document).ready(function() {
            let player = {
              position: {x: 275, y: 23},
              color: "black",
-            // lavirint_pozicija: {x : Math.round(this.position.x/15), y: Math.round(this.position.y/15)}
            }
 
 
            console.log(niz[0]);
-  //ctx.fillStyle = "#FF0000";
-  //ctx.fillRect(0, 0,context 200, 300);
+
   function  makeMaze() {
     for (var k = 0; k < 450; k=k+15) {
       for (var i = 0; i < 300; i = i+15) {
@@ -69,7 +71,7 @@ $(document).ready(function() {
 
   }
   makeMaze();
-  if (window.DeviceOrientationEvent) {
+/*  if (window.DeviceOrientationEvent) {
           window.addEventListener("deviceorientation", deviceOrientationListener);
         } else {
           alert("Sorry, your browser doesn't support Device Orientation");
@@ -98,5 +100,65 @@ $(document).ready(function() {
   }
 
 
+}*/
+window.addEventListener("keydown", tastatura);
+
+function tastatura() {
+  izracunavanje();
+  if (event.keyCode == 39) {
+    if (niz[poz_y][poz_x+1] == 1) {
+      if (ostatak_x < 8) {
+        promena_x(2);
+      }
+    }else {
+      promena_x(2);
+    }
+   makeMaze();
+  }
+  if (event.keyCode == 37) {
+    if (niz[poz_y][poz_x-1] == 1) {
+      if (ostatak_x > 7) {
+        promena_x(-2);
+      }
+    }else {
+      promena_x(-2);
+    }
+   makeMaze();
+  }
+//njnjnjnjnjnjnjn
+  if (event.keyCode == 40) {
+    if (niz[poz_y+1][poz_x] == 1) {
+      if (ostatak_y < 8) {
+        promena_y(2);
+      }
+    }else {
+      promena_y(2);
+    }
+
+    makeMaze();
+  }
+  if (event.keyCode == 38) {
+   if (niz[poz_y-1][poz_x] == 1) {
+      if (ostatak_y >= 8) {
+        promena_y(-2);
+      }
+    }else {
+      promena_y(-2);
+    }
+    makeMaze();
+  }
+ }
+function izracunavanje() {
+   poz_x =  Math.floor(player.position.x/15);
+   poz_y = Math.floor(player.position.y/15);
+   ostatak_x = player.position.x%15;
+   ostatak_y = player.position.y%15;
 }
+function promena_y(broj){
+  player.position.y+=broj;
+}
+function promena_x(nbroj){
+  player.position.x+=nbroj;
+}
+
 });
